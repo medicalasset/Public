d43fe96c71b118a2547fffb708ced3c5bc333d2e
Justin Jones <justin.nathaniel.jones@gmail.com>
2026-04-12 14:10:51 -0500

    main: For a friend

---
+++ Scripts/Python/ingredientOrganizer.py
@@ -0,0 +1,26 @@
+import csv
+
+def main():
+
+    ## Clearing/creating output file
+    with open("sampleOutput.txt", "w") as outputFile:
+        outputFile.write("")
+
+    with open("sampleInput.txt", newline="") as file:
+         fileReader = csv.reader(file, delimiter=",", quotechar='|')
+         conditionedInput = []
+         for line in fileReader:
+             with open("sampleOutput.txt", "a") as outputFile:
+                 ## Stripping the leading space from each ingredient
+                 ## otherwise the sorted method is inconsistent
+                 for ingredient in line:
+                     conditionedInput.append(ingredient.lstrip())
+                 sortedInput = sorted(conditionedInput)
+                 for ingredient in sortedInput:
+                     outputFile.write(ingredient + "\n")
+                 
+                 
+
+if __name__ == "__main__":
+
+    main()
